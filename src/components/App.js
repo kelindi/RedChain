a15df5291691d53c555f8caf4eefc17b0d5ad197
Kelindi <ayushk2020@gmail.com>
2021-02-17 19:33:02 -0500

    Added ether wallet check

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,19 @@ import Main from './Main'
 
 class App extends Component {
 
+  async loadWeb3(){
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You must use an Ethereum wallet to access this site. Consider using MetaMask!')
+    }
+  }
+
   constructor(props) {
     super(props)
     this.state = {
